f8c40d1b1e59ebdd0d57b061600b68e00e3466df
priyankarai11 <priyankakumbra@gmail.com>
2020-10-14 13:29:36 -0700

    Added:Calculating Daily wage of Employee

--- Employee.java
+++ Employee.java
@@ -4,6 +4,8 @@ public class Employee {
 	System.out.println("Welcome to Employee Wage Problem");
 	Random rand=new Random();
         int i=rand.nextInt(2);
+	int wageperHour=20;
+	int hourperDay=8;
         if (i == 1)
         {
                 System.out.println("Employee Present");
@@ -12,5 +14,9 @@ public class Employee {
         {
                 System.out.println("Employee Absent");
         }
+	System.out.println("****************************************");
+	System.out.println("Calculating Daily wage Of Employee");
+	int wageperDay=(wageperHour * hourperDay);
+	System.out.println("Daily Employee wage:"+wageperDay);
 }
 }
