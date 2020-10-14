84440724980394eefc6fdd024600b69fa701edde
priyankarai11 <priyankakumbra@gmail.com>
2020-10-14 13:34:24 -0700

    Added:Part time Employee and wage

--- Employee.java
+++ Employee.java
@@ -6,6 +6,7 @@ public class Employee {
         int i=rand.nextInt(2);
 	int wageperHour=20;
 	int hourperDay=8;
+	int parttimeHoursperDay=4;
         if (i == 1)
         {
                 System.out.println("Employee Present");
@@ -18,5 +19,11 @@ public class Employee {
 	System.out.println("Calculating Daily wage Of Employee");
 	int wageperDay=(wageperHour * hourperDay);
 	System.out.println("Daily Employee wage:"+wageperDay);
+
+	System.out.println("****************************************");
+        System.out.println("Calculating part time Employee and wage");
+        int parttimewageperDay=(wageperHour * parttimeHoursperDay);
+        System.out.println("Daily Employee wage:"+parttimewageperDay);
+
 }
 }
