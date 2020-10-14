1b6cfb71644a3025c37bbd70e76ff42ce5449a0d
priyankarai11 <priyankakumbra@gmail.com>
2020-10-14 14:49:09 -0700

    Added:Written class Method to compute Employee wage

--- Employee.java
+++ Employee.java
@@ -1,52 +1,64 @@
-import java.util.Scanner;
+import java.io.*;
 import java.util.Random;
-public class Employee {
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Problem");
-	Random rand=new Random();
-        int i=rand.nextInt(2);
-	int wageperHour=20;
-	int hourperDay=8,empHour=0,num=20;
-	String empName=" ";
-	int salary=0 , totalSal=0 , fullTime=1 , partTime=2;
-	int parttimeHoursperDay=4;
-        if (i == 1)
-        {
-                System.out.println("Employee Present");
-        }
-        else
-        {
-                System.out.println("Employee Absent");
-        }
-	System.out.println("****************************************");
-	System.out.println("Calculating Daily wage Of Employee");
-	int wageperDay=(wageperHour * hourperDay);
-	System.out.println("Daily Employee wage:"+wageperDay);
+import java.util.Scanner;
+public class Employee 
+{
+
+		public static final int wageperHour=20;
+                public static final int hourperDay=8;
+                public static final int parttimeHoursperDay=4;
+                public static final int fullTime=1 , partTime=2;
 
 
-        System.out.println("****************************************");
-        System.out.println("Enter number of working days");
-	System.out.println("Calculate wages for month");
-	//Scanner sc=new Scanner(System.in);
-	//int num=sc.nextInt();
-	int day=1;
-	while(day<=20 && empHour<=100)
-	{
-		int empCheck=rand.nextInt(2);
-		switch(empCheck){
-		case 0 :empName="Fulltime";
-			empHour+=8;
-			break;
-		case 1 :empName="Parttime";
-			empHour+=4;
-			break;
+        public static void empWage( )
+        {
+                System.out.println("Welcome to Employee Wage Problem");
+                Random rand=new Random();
+                String empName=" ";
+                int day=1,empHour=0,totalSal=0,salary;
+		int i=rand.nextInt(2);
+        	if (i == 1)
+        	{
+                	System.out.println("Employee Present");
+        	}
+        	else
+        	{
+                	System.out.println("Employee Absent");
+        	}
+        	System.out.println("****************************************");
+       		System.out.println("Calculating Daily wage Of Employee");
+        	int wageperDay=(wageperHour * hourperDay);
+        	System.out.println("Daily Employee wage:"+wageperDay);
+
+        	System.out.println("****************************************");
+        	System.out.println("Calculate wages for month");
+        	System.out.println("Employee wage");
+
+        	System.out.println("Enter number of working days");
+		while(day<=20 && empHour<=100)
+		{
+			int empCheck=rand.nextInt(2);
+			switch(empCheck){
+			case 0 :empName="Fulltime";
+				empHour=empHour+8;
+				break;
+			case 1 :empName="Parttime";
+				empHour=empHour+4;
+				break;
 		}
 		salary=(empHour*wageperHour);
 		System.out.println("Salary of " + empName + " on the " +day+ " is " +salary);
 		totalSal=(totalSal+salary);
 		day++;
 	}
-	System.out.println("Total Salary="+totalSal);
+	System.out.println("Total Employee Wage="+totalSal);
 
 }
+	public static void main(String[] args)
+	{
+		Employee empSal=new Employee();
+		empSal.empWage();
+	}
+
+
 }
