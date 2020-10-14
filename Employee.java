43c4443737941a2ffcc6959514bdd1c40c20ae2b
priyankarai11 <priyankakumbra@gmail.com>
2020-10-14 15:22:42 -0700

    Added:Save the Totalwage for each Company

--- Employee.java
+++ Employee.java
@@ -1,22 +1,30 @@
-import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 public class Employee 
-{
-	        public static final int wageperHour=20;
-                public static final int hourperDay=8;
-                public static final int parttimehoursperDay=4;
-                public static final int fullTime=1 , partTime=2;
+		
+{		public static  String company;
+	        public static  int empRatePerHour;
+                public static  int numOfWorkingDays;
+                public static  int maxHoursPerMonth;
+                public static  int fullTime=1 , partTime=2;
+		public static int totalWage;
 
 
-	public static void empWage(String Company,int wageperHour,int day,int empHour )
+	public  Employee(String company,int empRatePerHour, int numOfWorkingDays,int  maxHoursPerMonth){
+	this.company=company;
+	this. empRatePerHour= empRatePerHour;
+	this. numOfWorkingDays= numOfWorkingDays;
+	this. maxHoursPerMonth= maxHoursPerMonth;
+
+	}
+
+	public static void empWage( )
 	{
 		System.out.println("Welcome to Employee Wage Problem");
         	Random rand=new Random();
 		String empName=" ";
-		//String Company=" ";
-		//int day=1,empHour=0,
-		int totalSal=0,salary;
+		int empHr=0,days=0,totalEmpHrs=0;
+		int totalSal=0;
 
         	int i=rand.nextInt(2);
         	if (i == 1)
@@ -28,39 +36,45 @@ public class Employee
                 	System.out.println("Employee Absent");
         	}
         	System.out.println("****************************************");
-       		System.out.println("Calculating Daily wage Of Employee");
-        	int wageperDay=(wageperHour * hourperDay);
-        	System.out.println("Daily Employee wage:"+wageperDay);
+       		//System.out.println("Calculating Daily wage Of Employee");
+        	//int wageperDay=(wageperHour * hourperDay);
+        	//System.out.println("Daily Employee wage:"+wageperDay);
 
         	System.out.println("****************************************");
         	System.out.println("Calculate wages for month");
         	System.out.println("Employee wage");
-		int num=1;
+		//int num=1;
         	//System.out.println("Enter number of working days");
-		while(num<=day && num<=empHour)
+		while(totalEmpHrs<=maxHoursPerMonth && days<= numOfWorkingDays)
 		{
+			days++;
 			int empCheck=rand.nextInt(2);
 			switch(empCheck){
 			case 0 :empName="Fulltime";
-				empHour=empHour+8;
+				empHr=empHr+8;
 				break;
 			case 1 :empName="Parttime";
-				empHour=empHour+4;
+				empHr=empHr+4;
 				break;
 		}
-		salary=(empHour*wageperHour);
-		System.out.println("Salary of " + empName + " and Company name " +Company+ " on the " +num+ " is " +salary);
-		totalSal=(totalSal+salary);
-		num++;
+		totalEmpHrs=totalEmpHrs+empHr;
+		System.out.println("Days :"+ days + " Employee Hour :" +empHr);
+		}
+		totalWage=totalEmpHrs * empRatePerHour;
 	}
-	System.out.println("Total Employee Wage="+totalSal);
+	@Override
+	public String toString( ){
+	return "Total Employee Wage for Company :"+company+" is :"+totalWage;
+	}
 
-}
 	public static void main(String[] args)
 	{
-		empWage("BridgeLabz",20,2,10);
-		empWage("Wipro",10,4,20);
-
+		Employee bridgeLabz=new Employee("BridgeLabz",20,2,10);
+		bridgeLabz.empWage();
+		System.out.println(bridgeLabz);
+		Employee wipro=new Employee("Wipro",10,4,20);
+		wipro.empWage();
+		System.out.println(wipro);
 	}
 
 
